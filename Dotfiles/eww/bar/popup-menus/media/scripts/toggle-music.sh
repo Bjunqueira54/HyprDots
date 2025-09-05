7ada40d6693217a397af15be815e3d21a305ec58
Bjunqueira54 <dreadlock54@hotmail.com>
2025-09-05 11:09:27 +0100

    Removed forced monitor in the media window; Updated toggle-music script to open the media window on whatever monitor the mouse cursor currently is; Fix incorrect string in RAM usage widget; Misc organization work across bg-panel files; Tweaked weather widget to now fetch info every hour instead of every 2 hours;

--- Dotfiles/eww/bar/popup-menus/media/scripts/toggle-music.sh
+++ Dotfiles/eww/bar/popup-menus/media/scripts/toggle-music.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+SCREEN=0
+read CX CY <<< "$(hyprctl cursorpos | sed 's/,/ /')"
+
+if [[ $CX -lt 0 ]]; then
+	SCREEN=1
+elif [[ $CX -gt 1920 ]]; then
+	SCREEN=2
+fi
+
 if [[ -z $(eww active-windows | grep 'media') ]]; then
-	eww open media && eww update mediarev=true
+	eww open media --screen $SCREEN && eww update mediarev=true
 else
 	eww update mediarev=false
 	(sleep 0.2 && eww close media) &
